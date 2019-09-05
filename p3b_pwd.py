1c20397accf4f521cba80bbf3ccc02b4a3a8ec29
supermonstor99 <45298159+supermonstor99@users.noreply.github.com>
2019-09-05 12:43:04 +0530

    updated password program

--- p3b_pwd.py
+++ p3b_pwd.py
@@ -3,13 +3,18 @@ smallchar = 'abcdefghijklmnopqrstuvwxyz'
 bigchar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 splchar = '!@#$&%*^()'
 number = '1234567890'
-
+ctr = [0,0,0,0]
 def choose(stn):
     return stn[random.randint(0, len(stn)-1)]
+
 length = random.randint(8, 15)
 pwd = ''
 for x in range(0, length):
-    choice = random.randint(0,4)
+    while True:
+        avg = sum(ctr)/4
+        choice = random.randint(0,3)
+        if ctr[choice] <= avg:
+            break
     if(choice == 0):
         ch = choose(smallchar)
     elif(choice == 1):
@@ -18,6 +23,7 @@ for x in range(0, length):
         ch = choose(splchar)
     else:
         ch = choose(number)
+    ctr[choice] += 1
     pwd += ch
 
 print(pwd)
